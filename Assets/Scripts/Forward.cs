200654c4bbbc451f14fc8fd2b0128e2909d06f92
Razthgol <lucasmalvaso@gmail.com>
2016-09-03 12:55:38 -0300

    clase5 updates

--- Assets/Scripts/Forward.cs
+++ Assets/Scripts/Forward.cs
@@ -6,6 +6,9 @@ public class Forward : MonoBehaviour {
     public GameManagerBowl manager;
     private Rigidbody rigidbody;
 
+	private float force;
+
+
 	// Use this for initialization
 	void Start () {
         rigidbody = GetComponent<Rigidbody>();	
@@ -14,10 +17,22 @@ public class Forward : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.anyKeyDown) {
-            rigidbody.AddForce(new Vector3(Random.Range(-500, 500), Random.Range(-500, 500), 1000));
-            manager.start = true;
+		if (Input.GetKey(KeyCode.Space)) {
+			force += 50;
+			print (force);
         }
+
+		if (Input.GetKeyUp(KeyCode.Space)) {
+			rigidbody.AddForce (0,0,force);
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			rigidbody.AddForce (100,0,0);
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			rigidbody.AddForce (-100,0,0);
+		}
 	
 	}
 }
